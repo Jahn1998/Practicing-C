3403e60060206c41a2455f6545c54861b0f5120a
Jahn1998 <1713084222@qq.com>
2021-01-17 01:06:22 +0800

    scanf用法错误

--- Pointer8.21/Pointer8.21/main.c
+++ Pointer8.21/Pointer8.21/main.c
@@ -1,27 +1,29 @@
 //
 //  main.c
 //  Pointer8.21
-//
+//用指向指针的指针的方法对n个整数排序并输出。要求将排序单独写成一个函数。n个整数在主函数中输入，最后在主函数中输出。
 //  Created by 杨恩 on 2021/1/16.
-//12 2 4 2 3 1
+//12 2 4 2 3 1 87 90 89 0 43 23 4 1 2
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     void sort(int **p,int n);
     int num[20],*pnum[20];
-    int *p=num,*p_end,**pn=pnum,**pn_end,i,j;
-    scanf("%d",p++);
-    p_end=p;
-    p=num;
-    for(i=0;p<p_end;i++){
+    int *p,**pn=pnum,i,n;
+    n=10;
+    for(i=0;i<n;i++){
+        scanf("%d",&num[i]);
+    }
+    for(i=0;i<n;i++){
         pnum[i]= &num[i];
     }
-    pn_end=pn;
+    p=num;
     pn=pnum;
-    sort(pnum,i);
-    for(j=0;j<i;j++){
-        printf("%d ",*pnum[j]);
+    sort(pn,n);
+    for(i=0;i<n;i++){
+        printf("%d ",**pn);
+        pn++;
     }
     return 0;
 }
@@ -29,7 +31,7 @@ void sort(int **p,int n){
     int *temp,i,j;
     for(i=0;i<n-1;i++){
         for(j=i+1;j<n;j++){
-            if(**(p+j)>**(p+j)){
+            if(**(p+i)>**(p+j)){
                 temp=*(p+j);
                 *(p+j)=*(p+i);
                 *(p+i)=temp;
